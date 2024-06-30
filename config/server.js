5ec93e1cb60b87a9b976502d6e1b9159678eba80
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-30 11:33:53 -0300

    :tada: first commit

---
+++ config/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
+
+const app = express();
+
+// Configurações
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+// Middlewares
+app.use(express.static('./app/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Carregar rotas, modelos e controladores
+consign()
+  .include('./app/routes')
+  .then('./app/models')
+  .then('./app/controllers')
+  .into(app);
+
+// Exportar aplicativo
+module.exports = app;
