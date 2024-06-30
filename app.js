5cf4481f327b5d40eb138d87091eb792c2e57481
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-30 15:02:22 -0300

    :sparkles:	fix: adding socket

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const app = require('./config/server')
+const app = require('./config/server');
+const http = require('http');
+const server = http.createServer(app); // Create an HTTP server instance
 
-app.listen(80,() => {
+server.listen(80, () => {
     console.log('Servidor online');
-})
+});
+
+const io = require('socket.io')(server); // Initialize socket.io with the server instance
+
+// Optional: Handle socket connections
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado');
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado');
+    });
+});
