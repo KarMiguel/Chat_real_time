831051cf0c0363a9e5bb4d7621ff5076fdec5e74
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-30 23:44:46 -0300

    :sparkles: fix: adding logic chat real time

--- app/controllers/chatControllers.js
+++ app/controllers/chatControllers.js
@@ -1,6 +1,14 @@
 module.exports.iniciaChat = (application, req, res) => {
     try {
-        res.render('chat');
+        let dadosForm = req.body;
+        application.get('io').emit(
+            'msgParaCliente',
+            {
+                apelido: dadosForm.apelido,
+                mensagem: 'acabou de entrar no chat'
+            }
+        );
+        res.render('chat', { dadosForm: dadosForm });
     } catch (error) {
         console.error('Erro ao renderizar página de chat:', error);
         res.status(500).send('Erro interno ao iniciar o chat.');
