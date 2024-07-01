54f01b5422b30cd0d29f1f514f04d980b34211df
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-30 23:45:20 -0300

    :sparkles: fix: adding web socket.io chat

--- app.js
+++ app.js
@@ -8,11 +8,43 @@ server.listen(80, () => {
 
 const io = require('socket.io')(server); // Initialize socket.io with the server instance
 
-// Optional: Handle socket connections
+app.set('io',io);
+
 io.on('connection', (socket) => {
     console.log('Novo usuário conectado');
 
     socket.on('disconnect', () => {
         console.log('Usuário desconectado');
     });
+    socket.on('msgParaServidor',(data)=>{
+        //dialogo
+        socket.emit(
+            'msgParaCliente',
+            {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        )
+        socket.broadcast.emit(
+            'msgParaCliente',
+            {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        )
+
+
+      	/* participantes */
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+			socket.emit(
+				'participantesParaCliente', 
+				{apelido: data.apelido}
+			);
+
+			socket.broadcast.emit(
+				'participantesParaCliente', 
+				{apelido: data.apelido}
+			);
+		}
+    });   
 });
