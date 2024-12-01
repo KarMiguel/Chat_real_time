ddd030c4fb40e9dad465e7f5dd01003152abee2c
Carlos Miguel <119144240+KarMiguel@users.noreply.github.com>
2024-12-01 00:17:51 -0300

    a

--- config/server.js
+++ config/server.js
@@ -1,30 +1,23 @@
-const path = require('path');
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 // Configurações
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../app/views')); // Caminho absoluto para views
+app.set('views', path.join(__dirname, '../app/views'));
 
 // Middlewares
-app.use(express.static(path.join(__dirname, '../app/public'))); // Caminho absoluto para public
+app.use(express.static(path.join(__dirname, '../app/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Carregar rotas, modelos e controladores
+// Configurar e carregar rotas e controladores (sem 'models', já que a pasta não existe)
 consign({ cwd: path.join(__dirname, '../app') })
   .include('routes')
-  .then('models')
   .then('controllers')
-  .into(app)
-  .on('error', (err) => {
-    console.error('Erro ao carregar arquivos com consign:', err);
-  });
+  .into(app);
 
-console.log('Estrutura do objeto application:', app);
-
-
-// Exportar o servidor
+// Exporta o servidor configurado
 module.exports = app;
