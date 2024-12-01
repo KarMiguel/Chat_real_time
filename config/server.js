63504572a171dbf06e4e1fc1248d269cc352992d
Carlos Miguel <119144240+KarMiguel@users.noreply.github.com>
2024-12-01 00:12:11 -0300

    ajuste

--- config/server.js
+++ config/server.js
@@ -1,24 +1,24 @@
+const path = require('path');
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
-const { body, validationResult } = require('express-validator');
 
 const app = express();
 
 // Configurações
 app.set('view engine', 'ejs');
-app.set('views', './app/views');
+app.set('views', path.join(__dirname, '../app/views')); // Caminho absoluto para views
 
 // Middlewares
-app.use(express.static('./app/public'));
+app.use(express.static(path.join(__dirname, '../app/public'))); // Caminho absoluto para public
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Carregar rotas, modelos e controladores
-consign()
-  .include('./app/routes')
-  .then('./app/models')
-  .then('./app/controllers')
+consign({ cwd: path.join(__dirname, '../app') }) // Ajuste o diretório base do consign
+  .include('routes')
+  .then('models')
+  .then('controllers')
   .into(app);
 
-// Exportar aplicativo
+// Exportar o servidor
 module.exports = app;
