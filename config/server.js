c8cee45a162c595819111104668126ff80c0b868
Carlos Miguel <119144240+KarMiguel@users.noreply.github.com>
2024-12-01 00:15:10 -0300

    a

--- config/server.js
+++ config/server.js
@@ -14,11 +14,17 @@ app.use(express.static(path.join(__dirname, '../app/public'))); // Caminho absol
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Carregar rotas, modelos e controladores
-consign({ cwd: path.join(__dirname, '../app') }) // Ajuste o diretório base do consign
+consign({ cwd: path.join(__dirname, '../app') })
   .include('routes')
   .then('models')
   .then('controllers')
-  .into(app);
+  .into(app)
+  .on('error', (err) => {
+    console.error('Erro ao carregar arquivos com consign:', err);
+  });
+
+console.log('Estrutura do objeto application:', app);
+
 
 // Exportar o servidor
 module.exports = app;
